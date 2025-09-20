2ac9e94a141adeca6a77e71818f8930f9857d48f
ishowprogram <mohitgidhwani@gmail.com>
2025-09-20 20:46:41 +0530

    9/20/2025

---
+++ Loops/while loop/hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hello {
+    public static void main(String[] args) {
+        int i = 0; // Initialize counter variable
+
+        while (i < 5) {
+            System.out.println("Hello World");
+            i++; 
+        }
+    }
+}
